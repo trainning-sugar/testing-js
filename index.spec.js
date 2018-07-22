28348bdbcdb805301503a33fd59e5a409b9a232f
trainning-sugar <mlvilchezp@gmail.com>
2018-07-22 18:11:07 -0500

    cambiando el valor esperado del test 1 y verificando que sale error

--- index.spec.js
+++ index.spec.js
@@ -4,7 +4,7 @@ const {add} = require('./index');
 describe('The add function  test', () =>{
   it('Should return 11 for arguments 5 and 6 ', ()=>{
     const actual = add(5,6);
-    const expectation = 11;
+    const expectation = 12;
     assert.equal(actual, expectation);
   });
   it('Should return an error if arguments are not passed in', ()=>{
