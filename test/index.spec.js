48a6c95add89b41ebff12e91ec10012395be9ca8
trainning-sugar <mlvilchezp@gmail.com>
2018-07-22 21:26:01 -0500

    aplicando los estilos del documento

--- test/index.spec.js
+++ test/index.spec.js
@@ -9,24 +9,33 @@ describe('The package index.js', () => {
   describe('The parse function', () => {
     it(`Should return an object { by:'kati-frantz', popular:'true', category:'nodejs'} off all query params when '?by=kati-frantz&popular=true&category=nodejs' is pass to it`, () => {
       const actual = parse('?by=kati-frantz&popular=true&category=nodejs');
-      const expected = { by:'kati-frantz', popular:'true', category:'nodejs'};
+      const expected = {
+        by: 'kati-frantz',
+        popular: 'true',
+        category: 'nodejs'
+      };
       assert.deepEqual(actual, expected);
     });
     it(`Should return {number:'3'} when the query '?number=3' is pass to it`, () => {
-      assert.deepEqual(parse('?number=3'), {number:'3'});
+      assert.deepEqual(parse('?number=3'), {
+        number: '3'
+      });
     })
   });
   describe('The stringify function', () => {
     it(`Should return a string '?by=kati-frantz&popular=true&category=nodejs' when the object { by:'kati-frantz', popular:'true', category:'nodejs'} is pass to it `, () => {
-     const actual = stringify({by:'kati-frantz', popular:'true', category:'nodejs'});
-     const expected ='?by=kati-frantz&popular=true&category=nodejs';
-     assert.deepEqual(actual,expected);
+      const actual = stringify({
+        by: 'kati-frantz',
+        popular: 'true',
+        category: 'nodejs'
+      });
+      const expected = '?by=kati-frantz&popular=true&category=nodejs';
+      assert.deepEqual(actual, expected);
     });
     it(`Should return '?number=3' when object {number:3} is pass it`, () => {
-     assert.deepEqual(stringify({number:'3'}),'?number=3');
+      assert.deepEqual(stringify({
+        number: '3'
+      }), '?number=3');
     })
-
-
   })
-
 });
