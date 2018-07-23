b84a0a76ba64474e85a300bf8f560cbf387057af
trainning-sugar <mlvilchezp@gmail.com>
2018-07-22 21:24:53 -0500

    agregando el test para la cadena '?number=3' y el obj {number:'3'}

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,22 +5,28 @@ const {
 } = require('../src/index');
 
 describe('The package index.js', () => {
-  const str = '?by=kati-frantz&popular=true&category=nodejs';
-  const obj = { by:'kati-frantz', popular:'true', category:'nodejs'};
 
   describe('The parse function', () => {
-    it(`Should return an object ${obj} off all query params when ${str} is pass to it`, () => {
-      const actual = parse(str);
-      const expected = obj;
+    it(`Should return an object { by:'kati-frantz', popular:'true', category:'nodejs'} off all query params when '?by=kati-frantz&popular=true&category=nodejs' is pass to it`, () => {
+      const actual = parse('?by=kati-frantz&popular=true&category=nodejs');
+      const expected = { by:'kati-frantz', popular:'true', category:'nodejs'};
       assert.deepEqual(actual, expected);
     });
+    it(`Should return {number:'3'} when the query '?number=3' is pass to it`, () => {
+      assert.deepEqual(parse('?number=3'), {number:'3'});
+    })
   });
   describe('The stringify function', () => {
-    it(`Should return a string ${str} when a object ${obj} is pass to it `, () => {
-     const actual = stringify(obj);
-     const expected = str;
+    it(`Should return a string '?by=kati-frantz&popular=true&category=nodejs' when the object { by:'kati-frantz', popular:'true', category:'nodejs'} is pass to it `, () => {
+     const actual = stringify({by:'kati-frantz', popular:'true', category:'nodejs'});
+     const expected ='?by=kati-frantz&popular=true&category=nodejs';
      assert.deepEqual(actual,expected);
+    });
+    it(`Should return '?number=3' when object {number:3} is pass it`, () => {
+     assert.deepEqual(stringify({number:'3'}),'?number=3');
     })
+
+
   })
 
 });
