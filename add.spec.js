5a16bbd7a15abeb05068af2d70eb6bd8fa0af00b
trainning-sugar <mlvilchezp@gmail.com>
2018-07-22 19:05:40 -0500

    empezando el primer proyecto de test}

--- add.spec.js
+++
@@ -1,8 +0,0 @@
-const {add} = require('./index');
-const {test, assert} = require('./lib');
-
-test('The add functions adds two numbers', ()=>{
-  const actual = add(3,4);
-  const expected = 9;
-  assert.equal(actual, expected);
-})
